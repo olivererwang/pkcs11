3d1c2894d5baf541aef618efd1744110c1f60819
wangzhe <goddog2000@gmail.com>
2025-12-01 11:08:42 +0800

    修正BIP32导出公钥函数名称，更新相关文档注释

--- pkcs11f.h
+++ pkcs11f.h
@@ -876,12 +876,12 @@ CK_PKCS11_FUNCTION_INFO(C_DeriveKey)
  * The encoded key is stored in encodedKey (BIP32 serialization format)
  * if there were no errors.
  */
-CK_PKCS11_FUNCTION_INFO(CA_Bip32ExportPubKey)
+CK_PKCS11_FUNCTION_INFO(CA_Bip32ExportPublicKey)
 #ifdef CK_NEED_ARG_LIST
 (
   CK_SESSION_HANDLE    hSession,             /* session's handle */
   CK_OBJECT_HANDLE     hBaseKey,             /* base key */
-  CK_BYTE_PTR              encodedKey,       /* return value */
+  CK_CHAR_PTR              encodedKey,       /* return value */
   CK_ULONG_PTR             ulEncodedKeySize  /* extend key length */
 );
 #endif
